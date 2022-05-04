5483b50ec3350e98bf49771799dea093643e4ba3
Yoongdi <101148622+Yoongdi@users.noreply.github.com>
2022-05-04 11:14:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup (
   include_package_data=True,
-  name='mycalc0003',
+  name='output0003',
   version='0.0.3',
-  description='oss-dev my calculator 0003'
+  description='oss-dev my output 0003'
   author='yoongdi',
   author_email='2143504@donga.ac.kr',
   url ='https://github.com/Yoongdi/textoutput',
-  download_url = "https://githunhttps://github.com/yongdi/calc0003/archive/refs/tags/v.0.0.3zip",
+  download_url = "https://github.com/Yoongdi/textoutput",
   install_requires = ['pytest'],
    long_description = 'oss development ...',
    long_description_content_type = 'text/markdown',
