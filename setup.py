40d6d4968de5333c8f3300c70977b92b97a48510
Yoongdi <101148622+Yoongdi@users.noreply.github.com>
2022-05-04 10:49:52 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data=True,
+  name='mycalc0003',
+  version='0.0.3',
+  description='oss-dev my calculator 0003'
+  author='yoongdi',
+  author_email='2143504@donga.ac.kr',
+  url ='https://github.com/Yoongdi/textoutput',
+  download_url
+  
+  
+  
+  
+  
+  
+)
